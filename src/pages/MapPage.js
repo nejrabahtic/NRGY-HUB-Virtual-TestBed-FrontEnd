8612a6c530f6f702a8f35202c8437bda70bbbfaa
bella <bella@DESKTOP-P54Q61N>
2019-12-09 16:02:27 +0100

    starting with mainpage(map)

---
+++ src/pages/MapPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class MapPage extends Component{
+    render(){
+        return(
+         <h2>hej</h2>  
+
+        );
+    }
+}
+export default MapPage;

8ef620b5ff0dfe791b506a2e928a919cf0941aca
Donnis94 <dony-l.p@hotmail.it>
2019-12-09 17:24:56 +0100

    new component

---
+++ src/pages/homeMapCmp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class homeMapCmp extends Component {
+    render() {
+        return (
+            <div className="FormCenter">
+                <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+
+                </form>
+            </div>
+        );
+    }
+
+}
+export default homeMapCmp;

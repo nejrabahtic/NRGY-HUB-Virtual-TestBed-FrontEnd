897278779fd8ce039bee2e0f7486df5ee24e9e75
Clara Wiman <cwn17002@student.mdh.se>
2019-12-12 17:45:39 +0100

    save the values on the input

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SignInForm extends Component {
+
+  constructor(){
+
+    super();
+
+    this.state = {
+      name: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e){
+    let target = e.target;
+    let value = target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    console.log('the form');
+    console.log(this.state);
+  }
+
     render() {
         return (
             <div className="FormCenter">
-              <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+              <form className="FormFields" onSubmit={this.handleSubmit}>
                 <div className="FormField">
-                  <input type="text" id="name" className="FormField__Input" name="name" /* value={this.state.email} onChange={this.handleChange} */ />
+                  <input type="text" id="name" className="FormField__Input" name="name" value={this.state.name} onChange={this.handleChange} />
                   <label className="FormField__Label" htmlFor="email">Username</label>
                 </div>
 
                 <div className="FormField">
-                  <input type="password" id="password" className="FormField__Input" name="password" /* value={this.state.password} onChange={this.handleChange} */ />
+                  <input type="password" id="password" className="FormField__Input" name="password" value={this.state.password} onChange={this.handleChange} />
                   <label className="FormField__Label" htmlFor="password">Password</label>
                 </div>
 
--- src/pages/SignUpForm.js
+++ src/pages/SignUpForm.js
@@ -2,24 +2,56 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SignUpForm extends Component {
+
+  constructor(){
+
+    super();
+
+    this.state = {
+      name: '',
+      company: '',
+      email:'',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e){
+    let target = e.target;
+    let value = target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    console.log('the form');
+    console.log(this.state);
+  }
+
     render() {
         return (
         <div className="FormCenter">
             <form onSubmit={this.handleSubmit} className="FormFields">
               <div className="FormField">
-                <input type="text" id="name" className="FormField__Input"/* value={this.state.name} onChange={this.handleChange}*/ />
+                <input type="text" id="name" className="FormField__Input" name="name" value={this.state.name} onChange={this.handleChange} />
                 <label className="FormField__Label" htmlFor="name">Username</label>
               </div>
               <div className="FormField">
-                <input type="text" id="company" className="FormField__Input" name="company" /* value={this.state.email} onChange={this.handleChange} */ />
+                <input type="text" id="company" className="FormField__Input" name="company" value={this.state.company} onChange={this.handleChange} />
                 <label className="FormField__Label" htmlFor="company">Company Name</label>
               </div>
               <div className="FormField">
-                <input type="email" id="email" className="FormField__Input" name="email" /* value={this.state.email} onChange={this.handleChange} */ />
+                <input type="email" id="email" className="FormField__Input" name="email" value={this.state.email} onChange={this.handleChange} />
                 <label className="FormField__Label" htmlFor="email">E-mail</label>
               </div>
               <div className="FormField">
-                <input type="password" id="password" className="FormField__Input" name="password" /* value={this.state.password} onChange={this.handleChange} */ />
+                <input type="password" id="password" className="FormField__Input" name="password" value={this.state.password} onChange={this.handleChange} />
                 <label className="FormField__Label" htmlFor="password">Password</label>
               </div>
               <div className="FormField">

6bfad3112f83b98f4836071441ffcf405027e898
bella <bella@DESKTOP-P54Q61N>
2019-12-16 19:42:33 +0100

    imported navbar in mainView

--- src/components/mainView/mainView.js
+++ src/components/mainView/mainView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapView from '../mapComponent/mapView';
 import SideBar from '../sidebar/sidebar';
+import NavBar from '../navbar/navbar';
 
 class MainView extends Component {
     render() {

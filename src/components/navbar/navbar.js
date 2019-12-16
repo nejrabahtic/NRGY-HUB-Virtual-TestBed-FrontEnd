46ce428b627fb5e39615a25686844660fa361217
bella <bella@DESKTOP-P54Q61N>
2019-12-16 15:42:07 +0100

    started with navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './navbar.css';
+
+class navbar extends Component {
+
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default navbar;

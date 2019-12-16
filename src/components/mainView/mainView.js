42f1d36b7861f921a6dacce98d6f4cdb2b8169de
Clara Wiman <cwn17002@student.mdh.se>
2019-12-16 15:08:21 +0100

    start with sidebar

--- src/components/mainView/mainView.js
+++ src/components/mainView/mainView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import MapView from '../mapComponent/mapView';
+import SideBar from '../sidebar/sidebar';
 
 class MainView extends Component {
     render() {
         return (
             <div>
+                <SideBar/>
                 <MapView />
                 SOMETHING CRAZY
             </div>
---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import './sidebar.css';
+
+class SideBar extends Component {
+
+    render() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SideBar;

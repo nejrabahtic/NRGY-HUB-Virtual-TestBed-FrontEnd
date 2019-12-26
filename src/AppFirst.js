ef462fbdbc18cc853b25c3fcb262add715da7367
bella <bella@DESKTOP-P54Q61N>
2019-12-26 19:51:04 +0100

    layout size on reg/signin page for all desktop

--- src/AppFirst.js
+++ src/AppFirst.js
@@ -20,15 +20,18 @@ class AppWrong extends Component{
                 <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
                 <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
               </div> */}
+              
+              <div className="formContainer">
+                  <div className="FormTitle">
+                      <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                  </div>
 
-              <div className="FormTitle">
-                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                  <Route exact path="/" component={SignUpForm}>
+                  </Route>
+                  <Route path="/sign-in" component={SignInForm}>
+                  </Route>
               </div>
-
-              <Route exact path="/" component={SignUpForm}>
-              </Route>
-              <Route path="/sign-in" component={SignInForm}>
-              </Route>
+              
       
           </div>
 

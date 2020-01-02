905bda66bad3b1180a4813bc6edf8c46f541df62
Clara Wiman <cwn17002@student.mdh.se>
2020-01-02 21:50:49 +0100

    added markers on map with popup window

--- src/components/mapComponent/MapView.js
+++ src/components/mapComponent/MapView.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import './mapView.css';
+import { popup } from 'leaflet';
 
 let markers = [
     {
-      "lat": 59.6099,
-      "lng": 16.5447
+        lat: 59.6099,
+        lng: 16.5448,
+        message: "hello1"
     },
     {
-      "lat": 59.6097,
-      "lng": 16.5445
+        lat: 59.6098,
+        lng: 16.5447,
+        message: "hejdå"
     },
     {
-      "lat": 59.6096,
-      "lng": 16.5446
-    }
+        lat: 59.6098,
+        lng: 16.5446,
+        message: "h"
+    },
+    {
+        lat: 59.6095,
+        lng: 16.5447,
+        message: "helfer"
+      },
+      {
+        lat: 59.6094,
+        lng: 16.5445,
+        message: "huhwe"
+      },
+      {
+        lat: 59.6096,
+        lng: 16.5446,
+        message: "hdhewåoi"
+      }
  ];
 
 class MapView extends Component {
@@ -23,6 +42,7 @@ class MapView extends Component {
         this.state = {
             lat: 59.6099,
             lng: 16.5448,
+            message: "standrard",
             zoom: 12
         }
     }
@@ -36,11 +56,11 @@ class MapView extends Component {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
                     />
-                <Marker position={position}>
-                    <Popup>
-                        Vasteras center
-                    </Popup>
-                </Marker>
+                {markers.map(mark => (
+                    <Marker position={[mark.lat,mark.lng]}>
+                        <Popup>{mark.message}</Popup>
+                    </Marker>
+                ))}
                 </Map>
             </div>
         );
